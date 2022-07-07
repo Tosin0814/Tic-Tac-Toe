420ade2090a84d2db12c2529aae7e4ab42e177bb
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-07-06 23:25:13 -0400

    Removed Player vs CPU idea & cleaned up JS

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,10 @@
-const introPage = document.querySelector('#intro-page');
-const playerNum = document.querySelector('.player-num');
-const singlePlayer = document.querySelector('#single-player');
-const multiPlayer = document.querySelector('#multi-player');
+const introPage = document.querySelector('.intro-page');
+const start = document.querySelector('.start');
+const playerNames = document.querySelector('.player-names');
 const playerName1 = document.querySelector('.player1');
 const playerName2 = document.querySelector('.player2');
-const playerName3 = document.querySelector('.player3');
+const submitPlayerNames = document.querySelector('.submit-player-name');
 const playerScores = document.querySelectorAll('.player-score');
-const submitPlayerNameSingle = document.querySelector('#submit-player-name-single');
-const submitPlayerNameMulti = document.querySelector('#submit-player-name-multi');
 const mainContent = document.querySelector('.main-content');
 const displayPlayerName1 = document.querySelector('.disp-player1');
 const displayPlayerName2 = document.querySelector('.disp-player2');
@@ -15,17 +12,13 @@ const boardSpots = document.querySelectorAll('.spot');
 const winMessage = document.querySelector('.win');
 const resetButton = document.querySelector('.reset');
 
-// Player vs Player (1) or Player vs Computer (-1)
-let gameType = 0;
-
 let introButton = null;
 let currentSpot = null;
 let playerTurn = 1;
 
 
-
 // Class to create player objects
-class Player {
+class Players {
     constructor(name, score, choices){
         this.name = name;
         this.score = score;
@@ -34,9 +27,8 @@ class Player {
 }
 
 // Player Objects
-const playerOne = new Player('', 0, []);
-const playerTwo = new Player('', 0, []);
-const computer = new Player('Big Mac', 0, []);
+const playerOne = new Players('', 0, []);
+const playerTwo = new Players('', 0, []);
 
 // Winning combinations for game
 const winCombos = [
@@ -52,79 +44,36 @@ const winCombos = [
 
 
 
+// Render enter player names page and hide start button
+const inputPlayerNames = ()=> {
+    playerNames.classList.remove('hide-content');
+    playerNames.classList.add('flex-ctr');
+    start.classList.add('hide-content');
+}
+
 // Add styling and modify HTML when it is player one's turn
 const playerOneTurn = () => {
-    if (gameType === 1) {
         displayPlayerName1.style.backgroundColor = 'lightgreen'
         displayPlayerName2.style.backgroundColor = ''
         displayPlayerName1.innerHTML = `${playerOne.name}'s Turn`
         displayPlayerName2.innerHTML = playerTwo.name
-    } else if (gameType === -1) {
-        displayPlayerName1.style.backgroundColor = 'lightgreen'
-        displayPlayerName2.style.backgroundColor = ''
-        displayPlayerName1.innerHTML = `${playerOne.name}'s Turn`
-        displayPlayerName2.innerHTML = computer.name
-    }
     
 }
 
 // Add styling and modify HTML when it is player two's turn
 const playerTwoTurn = () => {
-    if (gameType === 1) {
         displayPlayerName1.style.backgroundColor = ''
         displayPlayerName2.style.backgroundColor = 'lightgreen'
         displayPlayerName1.innerHTML = playerOne.name
         displayPlayerName2.innerHTML = `${playerTwo.name}'s Turn`
-    } else if (gameType === -1) {
-        displayPlayerName1.style.backgroundColor = ''
-        displayPlayerName2.style.backgroundColor = 'lightgreen'
-        displayPlayerName1.innerHTML = playerOne.name
-        displayPlayerName2.innerHTML = `${computer.name}'s Turn`
-    }
-    
 }
 
 // Restore default HTML and style if a player has won
 const resetNameAndColor = () => {
-    if (gameType === 1) {
         displayPlayerName1.style.backgroundColor = ''
         displayPlayerName2.style.backgroundColor = ''
         displayPlayerName1.innerHTML = playerOne.name
         displayPlayerName2.innerHTML = playerTwo.name
-    } else if (gameType === -1) {
-        displayPlayerName1.style.backgroundColor = ''
-        displayPlayerName2.style.backgroundColor = ''
-        displayPlayerName1.innerHTML = playerOne.name
-        displayPlayerName2.innerHTML = computer.name
-    }
-    
-}
-
-
-// Hide select game type, render enter player names page and set value of gameType
-const inputPlayerNames = (evt)=> {
-    if(evt.target.id === 'one-player'){
-        singlePlayer.classList.remove('hide-content');
-        singlePlayer.classList.add('flex-ctr');
-        playerNum.classList.add('hide-content');
-        gameType = -1
-    }else if(evt.target.id === 'two-player'){
-        multiPlayer.classList.remove('hide-content');
-        multiPlayer.classList.add('flex-ctr');
-        playerNum.classList.add('hide-content');
-        gameType = 1
-    }
-}
-
-// Render player names depending on game type
-const renderPlayerNames = () => {
-    if(gameType === -1){
-        playerOneTurn()
-        displayPlayerName2.innerHTML = computer.name;
-    }else if(gameType === 1){
-        playerOneTurn()
-        displayPlayerName2.innerHTML = playerTwo.name;
-    }
 }
 
 // Hide intro page and render game board
@@ -139,18 +88,13 @@ const renderBoard = () =>{
 const renderMainPage = (evt) => {
     introButton = evt.target
     renderBoard();
-    if(gameType === -1){
-        playerOne.name = playerName1.value;
-    }else if(gameType === 1){
-        playerOne.name = playerName2.value;
-        playerTwo.name = playerName3.value;
-    }
-    renderPlayerNames();
+    playerOne.name = playerName1.value;
+    playerTwo.name = playerName2.value;
+    playerOneTurn()
 }
 
 // Check player win based on gameType
 const checkWin = () => {
-    if (gameType === 1) {
         if(playerTurn === -1){
             winCombos.forEach(winCombo => {
                 if(playerOne.choices.includes(winCombo[0]) && 
@@ -180,76 +124,24 @@ const checkWin = () => {
                 }
             });
         }
-    } else if (gameType === -1) {
-        if(playerTurn === -1){
-            winCombos.forEach(winCombo => {
-                if(playerOne.choices.includes(winCombo[0]) && 
-                playerOne.choices.includes(winCombo[1]) && 
-                playerOne.choices.includes(winCombo[2])){
-                    winMessage.innerHTML = `${playerOne.name} WINS!`;
-                    playerOne.score++
-                    playerScores[0].innerHTML = playerOne.score;
-                    boardSpots.forEach((boardSpot) => {
-                        boardSpot.disabled = true
-                    });
-                    resetNameAndColor()
-                }
-            });
-        }else if(playerTurn === 1){
-            winCombos.forEach(winCombo => {
-                if(computer.choices.includes(winCombo[0]) && 
-                computer.choices.includes(winCombo[1]) && 
-                computer.choices.includes(winCombo[2])){
-                    winMessage.innerHTML = `${computer.name} WINS!`;
-                    computer.score++
-                    playerScores[1].innerHTML = computer.score;
-                    boardSpots.forEach((boardSpot) => {
-                        boardSpot.disabled = true
-                    });
-                    resetNameAndColor()
-                }
-            });
-        }
-    }
 }
 
 // Lets current player play and then switch turns
 const Turn = ()=>{
-    if (gameType === 1) {
         if(playerTurn === 1){
             playerOne.choices.push(currentSpot.innerHTML)
-            // console.log('PlayerOneChoices', playerOne.choices)
             currentSpot.classList.remove('hide-spot-text')
             currentSpot.innerHTML = 'X';
             playerTwoTurn();
             playerTurn = -1
         }else if(playerTurn === -1){
             playerTwo.choices.push(currentSpot.innerHTML)
-            // console.log('PlayerTwoChoices', playerTwo.choices)
             currentSpot.classList.remove('hide-spot-text')
             currentSpot.innerHTML = 'O';
             playerOneTurn()
             playerTurn = 1
         }
         checkWin()
-    } else if (gameType === -1) {
-        if(playerTurn === 1){
-            playerOne.choices.push(currentSpot.innerHTML)
-            // console.log('PlayerOneChoices', playerOne.choices)
-            currentSpot.classList.remove('hide-spot-text')
-            currentSpot.innerHTML = 'X';
-            playerTwoTurn();
-            playerTurn = -1
-        }else if(playerTurn === -1){
-            computer.choices.push(currentSpot.innerHTML)
-            // console.log('PlayerTwoChoices', playerTwo.choices)
-            currentSpot.classList.remove('hide-spot-text')
-            currentSpot.innerHTML = 'O';
-            playerOneTurn()
-            playerTurn = 1
-        }
-        checkWin()
-    } 
 }
 
 // Disable board spot if it has already been played on
@@ -257,7 +149,7 @@ const handleBoardClick = (evt) => {
     currentSpot = evt.target;
     if(currentSpot.disabled === false){
         Turn();
-        currentSpot.disabled = true;    
+        currentSpot.disabled = true;
     } else if(currentSpot.disabled === true){
         console.log('disabled')
     }
@@ -265,7 +157,6 @@ const handleBoardClick = (evt) => {
 
 // Reset board to restart game or play new round
 const clearBoard = () => {
-    if (gameType === 1) {
         let spotNum = 0
         boardSpots.forEach((boardSpot) => {
             boardSpot.innerHTML = spotNum;
@@ -278,31 +169,15 @@ const clearBoard = () => {
         winMessage.innerHTML = '';
         playerTurn = 1;
         playerOneTurn()
-    } else if (gameType === -1) {
-        let spotNum = 0
-        boardSpots.forEach((boardSpot) => {
-            boardSpot.innerHTML = spotNum;
-            boardSpot.classList.add('hide-spot-text')
-            boardSpot.disabled = false;
-            spotNum++;
-        });
-        playerOne.choices = [];
-        computer.choices = [];
-        winMessage.innerHTML = '';
-        playerTurn = 1;
-        playerOneTurn()
-    }
 }
 
 
 
 
-//Single or Multiplayer?
-playerNum.addEventListener('click', inputPlayerNames);
+//Single or playerNames?
+start.addEventListener('click', inputPlayerNames);
 
-submitPlayerNameSingle.addEventListener('click', renderMainPage);
-
-submitPlayerNameMulti.addEventListener('click', renderMainPage);
+submitPlayerNames.addEventListener('click', renderMainPage);
 
 boardSpots.forEach((boardSpot) => {
     boardSpot.disabled = false
